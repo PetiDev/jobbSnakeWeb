aeba2eecbebcbd3774f2d06ff41e870ba5f57b9b
PetiDev <peteristvanh@gmail.com>
2023-05-08 11:49:11 +0200

    feat: :sparkles: basic snake

--- script.js
+++ script.js
@@ -5,27 +5,75 @@ const ctx = c.getContext("2d");
 ctx.canvas.width = c.clientWidth;
 ctx.canvas.height = c.clientHeight;
 
+let game;
+let inGame = false;
 
-const NodeSize = ctx.canvas.width/15;
+const NodeSize = ctx.canvas.width / 15;
 class Node {
-    color = clrGreen;
+    #color = clrGreen;
     constructor(x, y) {
         this.x = x;
         this.y = y;
     }
     draw() {
-        ctx.strokeStyle = this.color;
-        ctx.fillStyle = this.color;
+        ctx.strokeStyle = this.#color;
+        ctx.fillStyle = this.#color;
         ctx.fillRect(this.x, this.y, NodeSize, NodeSize);
     }
 }
 
-let node = new Node(100, 100);
+class Snake {
+    #nodes = [];
+    direction = "right";
+    constructor() {
+        this.#nodes.push(new Node(NodeSize, NodeSize));
+        this.#nodes.push(new Node(0, NodeSize))
+    }
+    draw() {
+        this.#nodes.forEach(n => {
+            n.draw()
+        })
+        this.move();
+    }
+    move() {
+        let lastNode = this.#nodes.pop()
+        switch (this.direction) {
+            case "up":
+                lastNode.y = this.#nodes[0].y - NodeSize;
+                this.#nodes.unshift(lastNode);
+                break;
+
+            case "down":
+                lastNode.y = this.#nodes[0].y + NodeSize;
+                this.#nodes.unshift(lastNode);
+                break;
+
+            case "left":
+                lastNode.x = this.#nodes[0].x - NodeSize;
+                this.#nodes.unshift(lastNode);
+
+            case "right":
+                lastNode.x = this.#nodes[0].x + NodeSize;
+                this.#nodes.unshift(lastNode);
+
+            default:
+                break;
+        }
+    }
+}
 
 
+
+let snake = new Snake();
 
 ctx.strokeStyle = clrGreen;
 ctx.fillStyle = clrGreen;
 
-ctx.strokeRect(0, 0, c.clientWidth, c.clientHeight)
-node.draw();
+
+game = setInterval(() => {
+    if(!inGame)clearInterval(game);
+    ctx.clearRect(0, 0, c.clientWidth, c.clientHeight)
+    ctx.strokeRect(0, 0, c.clientWidth, c.clientHeight)
+    snake.draw();
+}, 500);
+
